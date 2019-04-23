3dd3588f9d53f99e5de6c2b6b8dd870d41141063
Daniel Harvey <dhmillsy6@gmail.com>
2019-04-23 18:24:56 +0100

    Initial Commit

---
+++ Wacky Races v1 MAC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Wacky Races v1 MAC
+//
+//  Created by Daniel Harvey on 23/04/2019.
+//  Copyright © 2019 Daniel Harvey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
